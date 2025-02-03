e76d19cc20f2b2b961535f56e178960169f837b4
vinciis <vinciis2018@gmail.com>
2025-02-03 12:55:39 +0530

    backend url update

--- src/components/molecules/CampaignsListModel.tsx
+++ src/components/molecules/CampaignsListModel.tsx
@@ -1,4 +1,4 @@
-import { convertDataTimeToLocale } from "../../utils/dateAndTimeUtils"
+import { convertDataTimeToLocale, getNumberOfDaysBetweenTwoDates } from "../../utils/dateAndTimeUtils"
 import { generateColorFromAlphabet } from "../../utils/colorUtils"
 import clsx from "clsx"
 
@@ -47,6 +47,12 @@ export const CampaignsListModel = ({data}: any) => {
           <i className="fi fi-br-hourglass-end text-[#EF4444] text-[12px] flex items-center"></i>
           <h1 className="text-[12px] text-[#68879C]">
             : {convertDataTimeToLocale(data?.endDate)}
+          </h1>
+        </div>
+        <div className="flex gap-1 items-center justify-start">
+          <i className="fi fi-sr-pending text-[#06B6D4] text-[12px] flex items-center"></i>
+          <h1 className="text-[12px] text-[#06B6D4]">
+            : {getNumberOfDaysBetweenTwoDates(new Date(), data?.endDate) > 0 ? `` : ``}
           </h1>
         </div>
       </div>
--- src/pages/MyPlansListPage/MyPlansList.tsx
+++ src/pages/MyPlansListPage/MyPlansList.tsx
@@ -1,6 +1,4 @@
-import { TabWithoutIcon } from "../../components/index";
-import { PrimaryButton } from "../../components/atoms/PrimaryButton";
-import { PrimaryInput } from "../../components/atoms/PrimaryInput";
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SkeletonLoader } from "../../components/molecules/SkeletonLoader";
