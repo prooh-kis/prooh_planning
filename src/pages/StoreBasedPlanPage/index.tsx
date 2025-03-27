8e68b0a7a7294d459b2d0c0fb7bf3f5232551406
vinciis <vinciis2018@gmail.com>
2025-03-27 18:22:21 +0530

    trigger based flow

--- src/pages/StoreBasedPlanPage/index.tsx
+++ src/pages/StoreBasedPlanPage/index.tsx
@@ -67,7 +67,7 @@ export const StoreBasedPlanPage: React.FC = () => {
         storeBasePlanData.find(
           (page: any) => page.value === campaignDetails.currentPage
         )?.id || 0;
-        setCurrentStep(newStep >= steps ? newStep : newStep == 1 ? newStep + 1 : newStep + 1);
+      setCurrentStep(newStep >= steps ? newStep : newStep == 1 ? newStep + 1 : newStep + 1);
       const currStep = {
         [campaignId]: newStep >= steps ? newStep : newStep == 1 ? newStep + 1 : newStep + 1,
       };
