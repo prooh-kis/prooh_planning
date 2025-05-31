d043d815ac884ee992e3efac0118c3343163a1db
vishalkumar70522 <vishal.kumar@prooh.ai>
2025-05-31 16:49:49 +0530

    fixed add small chnages

--- src/components/layout/PrivateLayout/index.tsx
+++ src/components/layout/PrivateLayout/index.tsx
@@ -7,13 +7,16 @@ export const PrivateLayout = (props: any) => {
   const navigate = useNavigate();
   const { children } = props;
   const { userInfo } = useSelector((state: any) => state.auth);
+  const padding = props.padding || "pt-16";
 
   if (!userInfo) navigate(AUTH);
 
   return (
     <div className="h-[100vh] w-full overflow-y-auto no-scrollbar">
       <Header />
-      <div className="h-[92vh] overflow-y-auto no-scrollbar px-8 border-t pt-16">
+      <div
+        className={`h-[92vh] overflow-y-auto no-scrollbar px-8 border-t ${padding}`}
+      >
         {children}
       </div>
     </div>
